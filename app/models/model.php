7bedf37b2a9129c4f6439d10bbc275cca592ee3d
Junior <duartejusanca@gmail.com>
2020-11-30 15:49:16 +0100

    mis a jus de 30/11/20

--- app/models/model.php
+++ app/models/model.php
@@ -4,6 +4,7 @@ function dbConnect()
 {
     try {
         return new PDO("{$_ENV['DB_CONNECTION']}:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}", $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+
     } catch (Exception $e) {
         die($e->getMessage());
     }
--- db/migrations/20201130134958_my_new_migration.php
+++ db/migrations/20201130134958_my_new_migration.php
@@ -1,18 +1,19 @@
 <?php
-
 declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class CreatePostsTable extends AbstractMigration
+final class MyNewMigration extends AbstractMigration
 {
+
     public function change(): void
     {
         $table = $this->table('posts');
-        $table->addColumn('created_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
-            ->addColumn('updated_at', 'datetime', ['update' => 'CURRENT_TIMESTAMP'])
+        $table->addColumn('user_id', 'integer')
+            ->addColumn('updated_at', 'datetime', ['default' => 'CURRENT_TIMESTAMP'], ['update' => 'CURRENT_TIMESTAMP'])
             ->addColumn('title', 'string')
             ->addColumn('body', 'text')
             ->create();
     }
 }
+
